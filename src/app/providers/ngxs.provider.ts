bf732d0dc033d5d76d216eddc8b7ce87b59ab438
Sukhpreet Singh <panesar.pb08@gmail.com>
2024-03-26 02:24:33 -0400

    refactor

--- src/app/providers/ngxs.provider.ts
+++ src/app/providers/ngxs.provider.ts
@@ -1,13 +1,11 @@
 import { EnvironmentProviders, importProvidersFrom } from "@angular/core";
 import { NgxsModule } from '@ngxs/store';
 import { BoardsState } from "../store/boards/boards.state";
-import { NgxsLoggerPluginModule } from "@ngxs/logger-plugin";
 
 export function provideNgxs(): EnvironmentProviders {
   return importProvidersFrom(
     NgxsModule.forRoot([
       BoardsState,
     ]),
-    NgxsLoggerPluginModule.forRoot(),
   );
 }

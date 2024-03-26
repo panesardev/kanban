2738342e23e0f887653e475e071b67e696cf2c71
Sukhpreet Singh <panesar.pb08@gmail.com>
2024-03-26 02:39:40 -0400

    refactor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,24 +1,29 @@
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { PreloadAllModules, provideRouter, withComponentInputBinding, withInMemoryScrolling, withPreloading } from '@angular/router';
 import { routes } from './app.routes';
 import { provideFirebase } from './providers/firebase.provider';
 import { provideNgxs } from './providers/ngxs.provider';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(
-      routes,
-      withComponentInputBinding(),
-      withPreloading(PreloadAllModules),
-      withInMemoryScrolling({
-        scrollPositionRestoration: 'enabled',
-      }),
-    ), 
+      routes, 
+			withComponentInputBinding(), 
+			withPreloading(PreloadAllModules), 
+			withInMemoryScrolling({
+      	scrollPositionRestoration: 'enabled',
+    	}),
+		),
     provideClientHydration(),
     provideHttpClient(withFetch()),
     provideFirebase(),
     provideNgxs(),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
 };

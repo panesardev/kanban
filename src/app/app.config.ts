d586267f60bf52cb20cf89227954fc295daf7c3c
Sukhpreet Singh <panesar.pb08@gmail.com>
2024-03-28 20:21:32 -0400

    refactor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,11 @@
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { ApplicationConfig, isDevMode } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { provideClientHydration } from '@angular/platform-browser';
 import { PreloadAllModules, provideRouter, withComponentInputBinding, withInMemoryScrolling, withPreloading } from '@angular/router';
 import { routes } from './app.routes';
-import { provideFirebase } from './providers/firebase.provider';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,6 +19,20 @@ export const appConfig: ApplicationConfig = {
 		),
     provideClientHydration(),
     provideHttpClient(withFetch()),
-    provideFirebase(),
+    importProvidersFrom(
+      provideFirebaseApp(() => initializeApp(FIREBASE_CONFIG)),
+      provideFirestore(() => getFirestore()),
+      provideAuth(() => getAuth()),
+    )
   ],
+};
+
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyC01FiyNMH2PPvc7Tsdb572lABH6QVR4h8",
+  authDomain: "ngx-kanban.firebaseapp.com",
+  projectId: "ngx-kanban",
+  storageBucket: "ngx-kanban.appspot.com",
+  messagingSenderId: "602197176363",
+  appId: "1:602197176363:web:3dfc12be090408e7bafa6e",
+  measurementId: "G-4BBKE6GH7N"
 };

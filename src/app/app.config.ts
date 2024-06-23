dc95b5b1e2db60b2c45e000c6beaa537efce7173
Sukhpreet Singh <panesar.pb08@gmail.com>
2024-06-23 00:56:42 -0400

    refactor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
@@ -9,6 +9,7 @@ import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideExperimentalZonelessChangeDetection(),
     provideRouter(
       routes, 
 			withComponentInputBinding(), 
@@ -19,20 +20,16 @@ export const appConfig: ApplicationConfig = {
 		),
     provideClientHydration(),
     provideHttpClient(withFetch()),
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(FIREBASE_CONFIG)),
-      provideFirestore(() => getFirestore()),
-      provideAuth(() => getAuth()),
-    )
+    provideFirebaseApp(() => initializeApp({
+      apiKey: "AIzaSyC01FiyNMH2PPvc7Tsdb572lABH6QVR4h8",
+      authDomain: "ngx-kanban.firebaseapp.com",
+      projectId: "ngx-kanban",
+      storageBucket: "ngx-kanban.appspot.com",
+      messagingSenderId: "602197176363",
+      appId: "1:602197176363:web:3dfc12be090408e7bafa6e",
+      measurementId: "G-4BBKE6GH7N"
+    })),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
   ],
-};
-
-const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyC01FiyNMH2PPvc7Tsdb572lABH6QVR4h8",
-  authDomain: "ngx-kanban.firebaseapp.com",
-  projectId: "ngx-kanban",
-  storageBucket: "ngx-kanban.appspot.com",
-  messagingSenderId: "602197176363",
-  appId: "1:602197176363:web:3dfc12be090408e7bafa6e",
-  measurementId: "G-4BBKE6GH7N"
 };
